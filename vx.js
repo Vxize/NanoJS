3604e701346f90e7003763236a3e7a1720195dd9
NoDesireMonk <1267069+nodesiremonk@users.noreply.github.com>
2026-05-29 14:21:12 -0500

    Update vx.js

--- vx.js
+++ vx.js
@@ -1,11 +1,22 @@
 const $ = (selector, byId = true) => byId ? document.getElementById(selector) : document.querySelector(selector);
 const $$ = selector => document.querySelectorAll(selector);
-const value = (selector, byId = true) => $(selector, byId).value;
-const checked = (selector, byId = true) => $(selector, byId).checked;
+const value = (selector, byId = true) => $(selector, byId)?.value;
+const radioValue = (name) => value('input[name="' + name + '"]:checked', false);
+const checkValue = (name) => Array.from($$('input[name="' + name + '"]:checked')).map(c => c.value);
+const checked = (selector, byId = true) => $(selector, byId)?.checked;
 const disable = (selector, byId = true) => $(selector, byId).disabled = true;
 const enable = (selector, byId = true) => $(selector, byId).disabled = false;
 const addClass = (selector, byId = true, ...className) => $(selector, byId).classList.add(...className);
 const removeClass = (selector, byId = true, ...className) => $(selector, byId).classList.remove(...className);
 const hide = (selector, byId = true) => addClass(selector, byId, 'hidden');
 const show = (selector, byId = true) => removeClass(selector, byId, 'hidden');
-const content = (selector, content = '', html = false, byId = true) => html ? $(selector, byId).innerHTML = content : $(selector, byId).textContent = content;
+const content = (selector, content = '', html = false, byId = true) => {
+  const el = $(selector, byId);
+  if (el)  html ? el.innerHTML = content : el.textContent = content;
+}
+const callApi = async (data, url = null) =>
+  await fetch(url || window.location.href, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+  });
